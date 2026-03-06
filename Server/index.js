49a6ab2a37221f317168ba1e0dd2938c0901a6ab
kartavyashishangiya1409 <kartavyashishangiya1409@gmail.com>
2026-03-06 12:38:44 +0530

    Updated CORS

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:8081",
+    origin: ["http://localhost:8081", "https://ks-auth-trial.vercel.app"],
     credentials: true,
   })
 );
